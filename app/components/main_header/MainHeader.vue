97b5bcfb6533fe7162def81ba1cbf8d51eee2f5c
subta90 <haxyoy9990mk3@gmail.com>
2018-12-29 15:43:04 +0900

    add main_header.

---
+++ app/components/main_header/MainHeader.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <header class="hero is-info is-bold">
+      <div class="hero body">
+        <h1 class="title"> subta's portfolio.</h1>
+        <h2> Web Engineer </h2>
+      </div>
+    </header>
+  </div>
+</template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,66 +1,27 @@
 <template>
   <section class="container">
-    <div>
-      <logo/>
-      <h1 class="title">
-        subta-portfolio-front
-      </h1>
-      <h2 class="subtitle">
-        Nuxt.js project for subta portfolio
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
+    <div class="main-header">
+      <main-header/>
     </div>
   </section>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import MainHeader from '~/components/main_header/MainHeader.vue'
 
 export default {
   components: {
-    Logo
+    MainHeader
   }
 }
 </script>
 
 <style>
-
 .container {
   min-height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
   text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
 }
 </style>
